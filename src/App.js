20ecb1a0564053bd1300be554377ea8c725344b5
felipemoraesconcepta <felipe.moraes.c@conceptatech.com>
2023-01-15 18:44:20 -0300

    more

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <label>Berlo Store</label>
+        <div>
+          <label>Home</label>
+          <label>Quem somos</label>
+          <label>Home</label>
+          <label>Home</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
